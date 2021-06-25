799d3e271c7f5cba8517ada39377373ba8afe841
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-06-25 18:22:14 -0400

    test

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,7 +6,7 @@ export const About = (props) => {
           <div className="col-xs-12 col-md-6">
             {" "}
             <img
-              src="img/kaseem.jpg"
+              src={require("../imgs/kaseem.jpg")}
               className="img-responsive"
               alt="Kaseem"
             />{" "}
