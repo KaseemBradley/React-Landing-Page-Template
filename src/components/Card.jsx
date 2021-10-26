8c0729b7e171ff5141506f218c8995bd8aa97d1d
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-10-26 19:04:53 -0400

    flex props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,62 @@
 import "./Card.css";
 const Card = ({ title, imageName, body }) => {
   return (
-    <div className="card-container">
-      <div className="image-container">
-        <img src={`../img/${imageName}.jpg`} alt="todo" />
-      </div>
-      <div className="card-content">
-        <div className="card-title">
-          <h3>{title}</h3>
+    <div className="flex">
+      <div className="card-container">
+        <div className="image-container">
+          <img src={`../img/${imageName}.jpg`} alt="todo" />
         </div>
-        <div className="card-body">
-          <p>{body}</p>
+        <div className="card-content">
+          <div className="card-title">
+            <h3>{title}</h3>
+          </div>
+          <div className="card-body">
+            <p>{body}</p>
+          </div>
+        </div>
+        <div className="btn">
+          <button>
+            <a href="https://www.humanrightsfirst.org/asylum">View more</a>
+          </button>
         </div>
       </div>
-      <div className="btn">
-        <button>
-          <a href="https://www.humanrightsfirst.org/asylum">View more</a>
-        </button>
+
+      <div className="card-container">
+        <div className="image-container">
+          <img src={`../img/${imageName}.jpg`} alt="todo" />
+        </div>
+        <div className="card-content">
+          <div className="card-title">
+            <h3>{title}</h3>
+          </div>
+          <div className="card-body">
+            <p>{body}</p>
+          </div>
+        </div>
+        <div className="btn">
+          <button>
+            <a href="https://www.humanrightsfirst.org/asylum">View more</a>
+          </button>
+        </div>
+      </div>
+
+      <div className="card-container">
+        <div className="image-container">
+          <img src={`../img/${imageName}.jpg`} alt="todo" />
+        </div>
+        <div className="card-content">
+          <div className="card-title">
+            <h3>{title}</h3>
+          </div>
+          <div className="card-body">
+            <p>{body}</p>
+          </div>
+        </div>
+        <div className="btn">
+          <button>
+            <a href="https://www.humanrightsfirst.org/asylum">View more</a>
+          </button>
+        </div>
       </div>
     </div>
   );
